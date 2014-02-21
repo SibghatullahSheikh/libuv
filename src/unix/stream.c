e1b96afd19c7943f2be33ea249e9ebef0ce56591
Saúl Ibarra Corretgé <saghul@gmail.com>
2014-02-21 13:06:00 +0100

    Revert "stream: add an API to make streams do blocking writes"
    This reverts commit 92040eb7123e892261c8da71c3eedb97c00e4b88.
    
    Conflicts:
    	src/unix/stream.c
    	src/win/pipe.c

--- src/unix/stream.c
+++ src/unix/stream.c
@@ -1508,10 +1508,3 @@ void uv__stream_close(uv_stream_t* handle) {
 
   assert(!uv__io_active(&handle->io_watcher, UV__POLLIN | UV__POLLOUT));
 }
-
-
-int uv_stream_set_blocking(uv_stream_t* handle, int blocking) {
-  assert(0 && "implement me");
-  abort();
-  return 0;
-}
